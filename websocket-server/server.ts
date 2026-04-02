bc605326207dfab4e2e0ff38026c0adcc5220f6b
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-04-01 21:46:37 -0300

    feat(websocker-server): add websocket to server

--- websocket-server/server.ts
+++ websocket-server/server.ts
@@ -1,4 +1,5 @@
 import { Client, type ClientConfig } from "pg";
+import { WebSocketServer, type WebSocket } from "ws";
 
 const config: ClientConfig = {
   host: "localhost",
@@ -10,9 +11,25 @@ const config: ClientConfig = {
 
 const pgClient = new Client(config);
 
+const wss = new WebSocketServer({ port: 8001 });
+
+wss.on("connection", (ws: WebSocket) => {
+  console.log("Client connected");
+
+  ws.on("message", (data: Buffer) => {
+    console.log("Received:", data.toString());
+    ws.send(`Echo: ${data.toString()}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
 const connect = async (): Promise<void> => {
   await pgClient.connect();
   console.log("Postgres connected");
+  console.log("WebSocket server running on ws://localhost:8001");
 };
 
 connect();
