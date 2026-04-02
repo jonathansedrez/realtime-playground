0f2dc43f7153a1a4334770da9ae5a47059427e6d
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-04-02 15:36:27 -0300

    docs(websocket-server): move watch pg events to sql script

--- websocket-server/init.sql
+++ websocket-server/init.sql
@@ -3,3 +3,16 @@ CREATE TABLE IF NOT EXISTS issues (
   title VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE OR REPLACE FUNCTION notify_table_changes()
+RETURNS TRIGGER AS $$
+BEGIN
+  PERFORM pg_notify('table_changes', row_to_json(NEW)::text);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER issues_notify
+AFTER INSERT OR UPDATE ON issues
+FOR EACH ROW
+EXECUTE FUNCTION notify_table_changes();
